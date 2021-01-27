b0bb86f0f8cbb9299956c2bf5b14bb0b7ad44492
Morten <mtnielsen89@gmail.com>
2021-01-27 08:52:44 +0100

    Added meals to the meal categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,41 +1,46 @@
 import React from "react";
-import {View, Text, StyleSheet, Button} from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-import {CATEGORIES} from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-const CategoriesMealScreen = props =>{
-    const catId = props.navigation.getParam("categoryId");
-
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
-
+const CategoriesMealScreen = (props) => {
+  const renderMealItem = (itemData) => {
     return (
-        <View style={styles.screen}>
-            <Text>The CategoriesMealScreen</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="Go to Details!" onPress={() => {
-                props.navigation.navigate({routeName: "MealDetails"})
-            }}/>
-            <Button title="Go Back" onPress={() => {props.navigation.goBack()}}/>
-        </View>
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
     );
+  };
+
+  const catId = props.navigation.getParam("categoryId");
+
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  return (
+    <View style={styles.screen}>
+      <FlatList data={displayedMeals} renderItem={renderMealItem} />
+    </View>
+  );
 };
 
 CategoriesMealScreen.navigationOptions = (navigationData) => {
-    const catId = navigationData.navigation.getParam("categoryId");
+  const catId = navigationData.navigation.getParam("categoryId");
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
-    return {
-        headerTitle: selectedCategory.title,
-    };
+  return {
+    headerTitle: selectedCategory.title,
+  };
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default CategoriesMealScreen;
